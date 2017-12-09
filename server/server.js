26ecf25dd39d8f18b7a237e43690ae526a16f122
= <andrespechtbs@googlemail.com>
2017-12-09 19:53:49 +0100

    added get route for todos

--- server/server.js
+++ server/server.js
@@ -31,10 +31,18 @@ app.post('/todos', (req, res) => {
         });
 });
 
+app.get('/todos',(req, res) =>{
+    Todo.find({}).then((todos) =>{
+        res.send({todos});
+    },(err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
-    
+
 module.exports = {
     app
 }
