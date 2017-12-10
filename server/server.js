f48ad5a301e91338d92ad20fd37c5dfb70692ac8
= <andrespechtbs@googlemail.com>
2017-12-10 22:40:49 +0100

    add get:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {
     mongoose
@@ -36,6 +37,20 @@ app.get('/todos',(req, res) =>{
         res.send({todos});
     },(err) => {
         res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id',(req,res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    Todo.findById(req.params.id).then((todo) => {
+        if(!todo){
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
