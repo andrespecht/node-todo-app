22c70eac1b260226baf28cb5cb5ced81d45322b2
= <andrespechtbs@googlemail.com>
2017-12-21 20:53:12 +0100

    add Hash user passwd

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data,'123123dd');
-console.log(token);
+var password = 'andrewhatisgoingon';
 
-var decoded = jwt.verify(token,'123123dd');
-console.log(decoded);
-// var message = "I am to be encrypted";
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-// var hash = SHA256(message).toString();
+var hashedPassword = '$2a$10$WObIYeQYbrrG9NSKcMmfMuljx.vgJKxMh1CfmMUB285kczDK4u2my';
+
+bcrypt.compare(password, hashedPassword, (err,res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data,'123123dd');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123123dd');
+// console.log(decoded);
+// // var message = "I am to be encrypted";
+
+// // var hash = SHA256(message).toString();
 
 // console.log("message hashed is ",hash);
