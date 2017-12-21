408bb3d15ec8f38a814475ce993bc181b1da4045
= <andrespechtbs@googlemail.com>
2017-12-21 20:16:56 +0100

    add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ var {
     User
 } = require('./models/user');
 
+var {
+    authenticate
+} = require('./middleware/authenticate');
+
 var app = express();
 
 var port = process.env.PORT;
@@ -112,20 +116,35 @@ app.patch('/todos/:id', (req, res) => {
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
-    
+
     var user = new User({
         email: body.email,
         password: body.password
     });
 
-    user.save().then(() =>{
-        
+    user.save().then(() => {
+
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth',token).send(user);
-    }).catch((e) => { 
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    })
+
 });
 
 app.listen(port, () => {
