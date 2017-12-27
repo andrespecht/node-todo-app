49129947d6302a157115c3cd3fb8ac1834096b24
= <andrespechtbs@googlemail.com>
2017-12-27 20:51:23 +0100

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -37,33 +37,39 @@ var UserSchema = new mongoose.Schema({
 
 });
 
-UserSchema.methods.toJSON = function(){
+UserSchema.methods.toJSON = function () {
     var user = this;
 
     var userObject = user.toObject();
-    
-    return _.pick(userObject, ['_id','email']);
+
+    return _.pick(userObject, ['_id', 'email']);
 
 }
 
-UserSchema.methods.generateAuthToken = function(){
+UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-    
-    user.tokens.push({access, token});
-    
+    var token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, 'abc123').toString();
+
+    user.tokens.push({
+        access,
+        token
+    });
+
     return user.save().then(() => {
         return token;
     });
 }
 
-UserSchema.statics.findByToken = function(token){
+UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
 
     try {
-       decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, 'abc123');
 
     } catch (error) {
         return Promise.reject();
@@ -76,29 +82,51 @@ UserSchema.statics.findByToken = function(token){
     })
 }
 
-UserSchema.pre('save', function(next){
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({
+        email
+    }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user);
+                }
+                else{
+                    reject();
+                }
+            });
+
+        });
+    })
+}
+
+UserSchema.pre('save', function (next) {
     var user = this;
 
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
 
-        bcrypt.genSalt(10, (err, salt) =>{
-            bcrypt.hash(user.password, salt, (err, hash) =>{
-                user.password= hash;
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
                 next();
             });
         });
         // bcrypt.genSalt(10, (err, salt) => {
-//     bcrypt.hash(password, salt, (err, hash) => {
-//         console.log(hash);
-//     });
-// });
-    }
-    else{
+        //     bcrypt.hash(password, salt, (err, hash) => {
+        //         console.log(hash);
+        //     });
+        // });
+    } else {
         next();
     }
 });
 
-var User = mongoose.model('User',UserSchema );
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
     User
