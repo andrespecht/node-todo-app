d2388ac330d36b1069553dd8d35bb01c0d60e2d3
= <andrespechtbs@googlemail.com>
2017-12-28 20:10:51 +0100

    update config to use json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -22,7 +22,7 @@ const users = [{
             token: jwt.sign({
                 _id: userOneId,
                 access: 'auth'
-            }, 'abc123').toString()
+            }, process.env.JWT_SECRET).toString()
         }]
     },
     {
@@ -34,7 +34,7 @@ const users = [{
             token: jwt.sign({
                 _id: userTwoId,
                 access: 'auth'
-            }, 'abc123').toString()
+            }, process.env.JWT_SECRET).toString()
         }]
     }
 
